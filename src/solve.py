056d6a4b4a8ed56e978281184535604cbb8bf294
maweizheng <maweizheng@didiglobal.com>
2024-04-20 14:13:08 +0800

    no change

--- src/solve.py
+++ src/solve.py
@@ -4,7 +4,7 @@ from model_of_second_stage import *
 from uilts import *
 
 
-num_of_nodes = 30
+num_of_nodes = 50
 requests = read_demand_data(f"../data/demand_{num_of_nodes}.csv")
 travel_time = read_time_matrix(f"../data/time_matrix_{num_of_nodes}.csv")
 parameters = read_parameter(f"../data/parameter.csv")
@@ -14,14 +14,17 @@ instance = Instance(nodes, transfers, requests, travel_time, parameters)
 
 
 first_stage_model = Model(instance)
-first_stage_model.model.setParam('TimeLimit', 10)
+first_stage_model.model.setParam('TimeLimit', 120)
 first_stage_model.model.optimize()
 x = set()
 y = set()
 status = first_stage_model.model.status
 if status == gp.GRB.OPTIMAL or (status == gp.GRB.TIME_LIMIT and first_stage_model.model.SolCount > 0):
+    print("first stage cost")
+    print("travel_cost, ", first_stage_model.travel_cost.x)
+    print("transfer_cost, ", first_stage_model.transfer_cost.x)
+    print("fix vehicle_cost, ", first_stage_model.fixed_vehicle_cost.x)
     for (i, j) in first_stage_model.x:
-
         if first_stage_model.x[i, j].x > 0.5:
             x.add((i, j))
 
@@ -35,7 +38,7 @@ if status == gp.GRB.OPTIMAL or (status == gp.GRB.TIME_LIMIT and first_stage_mode
 
     second_stage_model = Schedule(e, l, pairs, instance, vehicles, arcs)
     second_stage_model.model.setParam('TimeLimit', 120)
-    second_stage_model.model.optimize()
+    # second_stage_model.model.optimize()
 
 else:
     print(status, first_stage_model.model.SolCount)
